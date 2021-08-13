94ab7ce1cac9100b5f2e2cbb9814ca26b26c3caa
wanjhen <a10923001@gemail.yuntech.edu.tw>
2021-08-13 17:01:40 +0800

    update some data

--- js/jquery.js
+++ js/jquery.js
@@ -1,40 +1,40 @@
 function postData(url, data) {
   return fetch(url, {
     body: JSON.stringify(data),
-    cache: "no-cache",
-    credentials: "same-origin",
+    cache: 'no-cache',
+    credentials: 'same-origin',
     headers: {
-      "user-agent": "Example",
-      "content-type": "application/json"
+      'user-agent': 'Example',
+      'content-type': 'application/json'
     },
-    method: "POST",
-    mode: "cors",
-    redirect: "follow",
-    referrer: "no-referrer",
+    method: 'POST',
+    mode: 'cors',
+    redirect: 'follow',
+    referrer: 'no-referrer',
   })
     .then(response => response.json())
 }
 
 function submit(){
-  const keyin = document.getElementById("keyin").value;
+  const keyin = document.getElementById('keyin').value;
   
   const data = {
     keyin
   }
   
-  postData("https://wanjhen.github.io/poetryai/", data)
+  postData('https://wanjhen.github.io/poetryai/', data)
   .then(data=>{
     const result = data.result;
     //console.log(result);
-    document.getElementById("resultText").innerHTML=result;
+    document.getElementById('resultText').innerHTML=result;
   })
 }
 
-var swiper = new Swiper(".mySwiper", {
+var swiper = new Swiper('.mySwiper', {
     slidesPerView: 7,
     spaceBetween: 5,
     pagination: {
-      el: ".swiper-pagination",
+      el: '.swiper-pagination',
       clickable: true,
     },
   });
