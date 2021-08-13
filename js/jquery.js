a954851adfbd0a943369ca91f40fd625affb85e7
wanjhen <a10923001@gemail.yuntech.edu.tw>
2021-08-13 16:19:19 +0800

    update some data

--- js/jquery.js
+++ js/jquery.js
@@ -1,3 +1,35 @@
+function postData(url, data) {
+  return fetch(url, {
+    body: JSON.stringify(data),
+    cache: 'no-cache',
+    credentials: 'same-origin',
+    headers {
+      'user-agent': 'Example',
+      'content-type': 'application/json'
+    },
+    method: 'POST',
+    mode: 'cors',
+    redirect: 'follow',
+    referrer: 'no-referrer',
+  })
+    .then(response => response.json())
+}
+
+function submit(){
+  const keyin = document.getElementById('keyin').value;
+  
+  const data ={
+    keyin
+  }
+  
+  postData('https://wanjhen.github.io/poetryai/', data)
+  .then(data=>{
+    const result = data.result;
+    //console.log(result);
+    document.getElementById('resultText').innerHTML=result;
+  })
+}
+
 var swiper = new Swiper(".mySwiper", {
     slidesPerView: 7,
     spaceBetween: 5,
